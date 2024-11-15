bb86609d97c378bd1e8aad6d88bfc17ca3dd3def
iamsuraj-01 <ssemwal181096@gmail.com>
2024-11-15 21:26:14 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -383,7 +383,7 @@
 
     })();
 
-    // Book a session modal
+    // Book a session modal 
     const modal = document.getElementById("bookSessionModal");
     const btn = document.getElementById("bookSessionBtn");
     const span = document.getElementsByClassName("close")[0];
